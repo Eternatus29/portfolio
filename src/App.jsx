e9a8ceea4f83b048a63eb1222947af68a4e96643
Anik Roy <anikroy173@gmail.com>
2024-06-03 07:30:12 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Header from './components/header/Header';
 import Home from './components/home/Home';
 import About from './components/about/About';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <main className='main'>
         <Home />
         <About />
+        <SpeedInsights />
       </main>
     </>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <SpeedInsights />
   </React.StrictMode>,
 )
